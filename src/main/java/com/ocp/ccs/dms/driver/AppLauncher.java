2fbcdf256f5b92feee7b6881efb57c11bc87a671
satbala1 <satbala1@yahoo.com>
2017-09-18 14:12:30 -0400

    adding to the master

---
+++ src/main/java/com/ocp/ccs/dms/driver/AppLauncher.java
@@ -0,0 +1,28 @@
+package com.ocp.ccs.dms.driver;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+import org.apache.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppLauncher {
+
+	private static final Logger LOG = Logger.getLogger(AppLauncher.class);
+	public static void main(String[] args) {
+		
+		//SpringApplication.run(AppLauncher.class, args);
+		
+		HashMap<Integer,String> hMap = new HashMap<Integer,String>();
+		hMap.put(1, "Amar");
+		hMap.put(2, "ObjectOriented");
+		System.out.println(hMap.get(1));
+		for(Entry m : hMap.entrySet()) {
+			System.out.println("bb " + m.getValue());
+		}
+	}
+
+	
+}
---
+++ src/test/java/com/spring/amar/interview/exercises/ExercisesApplicationTests.java
@@ -0,0 +1,26 @@
+package com.spring.amar.interview.exercises;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExercisesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void testAddNumbers() {
+		int actual = 0;
+		assertEquals(0, actual);
+	}
+	
+}
